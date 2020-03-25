ad9a96dcf2869fa92a13def9f4b50aa017d53b80
Nirag Mehta <mehtanirag@gmail.com>
2020-03-24 21:06:52 -0700

    config files

---
+++ newfrontend/src/config.js
@@ -0,0 +1,12 @@
+const config = {
+  cognito: {
+    userPoolId: "us-east-1_WvAf7TrH2", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "6i3irkjn90i5j6gqf19md40dnr", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-1" // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "" // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+  }
+};
+
+export default config;
