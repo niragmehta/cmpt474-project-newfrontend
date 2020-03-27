c6c44835e8b37619e168cb0ff7d2421370911ccc
root <root@Jordan-PC212451.localdomain>
2020-03-26 17:32:31 -0700

    Login page now lets you enter in text and updates state accordingly

--- newfrontend/src/components/Login.js
+++ newfrontend/src/components/Login.js
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
 //import { Link, Redirect } from "react-router-dom";
 
-export class Login extends Component {
-  fields = {
-    username: "",
-    password: ""
-  };
+export class Login extends React.Component {
 
-  updateVal(e) {
-    console.log(e);
-    //fields.username = { [e.target.name]: e.target.value };
+  constructor(props){
+    super(props);
+    this.state = {
+      username: 'defaultuser',
+      password: 'defaultps'
+    }
+
+    this.onChangeUser = this.onChangeUser.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
+  //Executes when you click the submit button.
   onSubmit = e => {
     e.preventDefault();
-    //this.props.login(this.state.username, this.state.password);
+    //this.props.login(this.state.username, this.state.password); //We don't have a login page yet...
   };
 
-  onChange = e => this.updateVal(e);
+  onChangeUser(event){
+    console.log(event.target.value) //debug purposes
+    this.setState({ username: event.target.value });
+  }
+
+  onChangePassword(event){
+    console.log(event.target.value)
+    this.setState({password: event.target.value})
+  }
 
   render() {
     return (
@@ -25,35 +37,44 @@ export class Login extends Component {
         <div className="card card-body mt-5">
           <h2 className="text-center">Login</h2>
           <form onSubmit={this.onSubmit}>
+
             <div className="form-group">
               <label>Username</label>
               <input
                 type="text"
                 className="form-control"
                 name="username"
-                value={this.fields.username}
-                onChange={this.onChange}
+                value={this.state.username}
+                onChange={this.onChangeUser}
               />
             </div>
+
             <div className="form-group">
               <label>Password</label>
               <input
                 type="password"
                 className="form-control"
                 name="password"
-                onChange={this.onChange}
-                value={this.fields.password}
+                value={this.state.password}
+                onChange={this.onChangePassword}
               />
             </div>
+
             <div className="form-group">
               <button type="submit" className="btn btn-primary">
                 Login
               </button>
             </div>
+
             <p>Don't have an account?</p>
           </form>
+
         </div>
       </div>
     );
+
+
+
+
   }
 }
