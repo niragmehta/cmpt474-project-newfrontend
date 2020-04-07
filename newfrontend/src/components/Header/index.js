8c8022e644a0b2b5e3c69811d85b0cc4ad9e3fb1
Jordan Hoang <jordanhoang@live.ca>
2020-04-06 22:27:37 -0700

    Header changes

--- newfrontend/src/components/Header/index.js
+++ newfrontend/src/components/Header/index.js
@@ -19,37 +19,45 @@ export default function ButtonAppBar(props) {
     console.log("Hello : " + props.username);
   };
 
+
   return (
-    <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
           <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <a className="nav-link btn btn-primary btn-block" href="#">
+            <a className="nav-link" href="#">
               Blog<span className="sr-only">(current)</span>
             </a>
-            <Link to="/aboutus" className="nav-link btn btn-warning btn-block">
+            <Link to="/aboutus" className="nav-link">
               About Us
             </Link>
-            <button
-              className="nav-link btn btn-danger btn-block"
-              onClick={handleOnSignOut}
-            >
+            <Link to="/countryinfo" className = "nav-link">
+              Country Info
+            </Link>
+            <Link to="/login" className = "nav-link" onClick={handleOnSignOut}>
               Sign Out
-            </button>
+            </Link>
+
+
+
           </div>
         </div>
       </nav>
-    </div>
   );
+
+
+
+
+
+
 }
