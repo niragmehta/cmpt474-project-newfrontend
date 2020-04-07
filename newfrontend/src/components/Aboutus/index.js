72358011333c57ca18748d8513c22e446e038658
Jordan <36338184+jordan-hoang@users.noreply.github.com>
2020-04-06 20:29:14 -0700

    Changes to aboutus, index.js (#3) Port of the old aboutus page

--- newfrontend/src/components/Aboutus/index.js
+++ newfrontend/src/components/Aboutus/index.js
@@ -1,13 +1,37 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Switch, Route, Redirect } from "react-router-dom";
+import {Switch, Route, Redirect, Link} from "react-router-dom";
+import Button from "@material-ui/core/Button";
+
 
 const Aboutus = (props) => {
-  return (
-    <div>
-      <div>About Us</div>
-    </div>
-  );
+
+    const listStyle = {
+        listStyleType:"lower-greek", // Change this to no-style if you want
+        color: "grey"
+    };
+
+    return (
+        <div class="jumbotron">
+            <h1 class="display-3">Hello, world!</h1>
+            <p class="lead">
+                The Connect People App is a web app designed to bring together people
+                of different countries and cultures where information and experience
+                can be shared in a friendly and supportive environment.
+            </p>
+            <hr class="my-4" />
+            <p>
+                The team is as follows
+                <ul style={listStyle}>
+                    <li>Nirag Mehta </li>
+                    <li>Micheal Wong</li>
+                    <li>Elias</li>
+                    <li>Archer Zhi</li>
+                    <li>Jordan Hoang</li>
+                </ul>
+            </p>
+        </div>
+    );
 };
 
 Aboutus.propTypes = {};
